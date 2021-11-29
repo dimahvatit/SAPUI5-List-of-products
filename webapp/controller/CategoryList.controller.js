4df8d8de499e620a94d7416c83ef69f80e268d14
Dima <katorgin.dmitry@gmail.com>
2021-11-29 14:18:22 +0300

    change onPress in list controller

--- webapp/controller/CategoryList.controller.js
+++ webapp/controller/CategoryList.controller.js
@@ -39,15 +39,14 @@ function (Controller, Filter, FilterOperator) {
         },
 
         onPress: function (oEvent) {
-            console.log(oEvent.getSource().getBindingContext("category").getObject());
-            console.log(oEvent.getSource().getBindingContext("category"));
 
-            let sPath = oEvent.getSource().getBindingContext("category").getPath().substr(1);
+            // let sPath = oEvent.getSource().getBindingContext("category").getPath().substr(1);
+            let ID = oEvent.getSource().getBindingContext("category").getObject().CategoryID;
 
             let oRouter = this.getOwnerComponent().getRouter();
 
             oRouter.navTo('products', {
-                productPath: window.encodeURIComponent(sPath)
+                productPath: window.encodeURIComponent(`Categories(${ID})`)
             });
         },
     });
