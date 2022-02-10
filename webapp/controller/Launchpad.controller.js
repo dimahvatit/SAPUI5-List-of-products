77f1fc1013c2d2836d694ed3b79d1507bc2da941
dimahvatit <katorgin.dmitry@gmail.com>
2022-02-10 19:14:31 +0300

    improve tile updating logic

--- webapp/controller/Launchpad.controller.js
+++ webapp/controller/Launchpad.controller.js
@@ -4,36 +4,133 @@ sap.ui.define([
 	'use strict';
 
 	return BaseController.extend('my_cat_list.controller.Launchpad', {
-		onInit: function () {
+		onInit() {
 			this.aFlexItems = this.getView().byId('flexContainer').getItems();
 			this.aButtons = this.getView().byId('btnContainer').getItems();
 			this.oCartModel = this.getOwnerComponent().getModel('cartProducts');
 			this.sBaseUrl = 'http://localhost:8081';
 			this.getView().setModel(this.oCartModel, 'cart');
 
-			let dynamicTiles = [
-				{
-					id: 'orders-tile-count',
-					timer: 5,
+			let dynamicTilesCounters = {
+				'orders-tile-count': {
 					route: 'orders',
+					timer: 2,
 				},
-				{
-					id: 'suppliers-tile-count',
-					timer: 1,
+				'suppliers-tile-count': {
 					route: 'suppliers',
+					timer: 6,
 				},
-			];
+			};
 
-			this._refreshTiles(dynamicTiles);
-
-			/* this._fetchData('orders', 'orders-tile-count', 3);
-			this._fetchData('suppliers', 'suppliers-tile-count', 5); */
+			this._refreshTiles(dynamicTilesCounters);
 		},
 
-		onDrop: function (oEvent) {
-			let oTileContainer = oEvent.getSource().getParent();
+		/**
+		 * Refreshes tiles' counters each second according to their individual timers
+		 * @param {object} tiles
+		 */
+		_refreshTiles(tiles) {
+			let timers = [];
+			let aToUpdate = [];
+			let counter = 1;
 
-			var oDragged = oEvent.getParameter("draggedControl"),
+			for (const key in tiles) {
+				if (Object.hasOwnProperty.call(tiles, key)) {
+					const element = tiles[key];
+					timers.push(element.timer);
+				}
+			}
+
+			setInterval(() => {
+				for (const key in tiles) {
+					if (Object.hasOwnProperty.call(tiles, key)) {
+						const el = tiles[key];
+
+						// push id of the control that must be updated at current moment
+						if (counter % el.timer == 0) {
+							aToUpdate.push(key);
+						}
+					}
+				}
+
+				getData();
+
+				if (counter >= Math.max(...timers)) {
+					counter = 1;
+				} else {
+					counter++;
+				}
+			}, 1000);
+
+			let that = this;
+			function getData() {
+				// shuffle the array so the tiles are updated in random order
+				that._shuffle(aToUpdate);
+				aToUpdate.forEach((el, i) => {
+					setTimeout(() => {
+						that._updateData(tiles[el].route, el);
+						console.log(el + ' update');
+					}, 500 * i);
+				})
+				aToUpdate.length = 0;
+			}
+		},
+
+		/**
+		 * Fisher–Yates shuffle (changes the original array)
+		 */
+		_shuffle: function(array) {
+			let currentIndex = array.length,  randomIndex;
+
+			// While there remain elements to shuffle...
+			while (currentIndex != 0) {
+
+				// Pick a remaining element...
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex--;
+
+			  // And swap it with the current element.
+				[array[currentIndex], array[randomIndex]] = [
+					array[randomIndex], array[currentIndex]];
+			}
+
+			return array;
+		},
+
+		/**
+		 * Fetch data from express server
+		 *  @param {string} route - route to request data from
+		 *  @param {object} oControl - control which 'text' attr will get the fetched data
+		 */
+		_updateData(route, sControlId) {
+			let oControl = this.getView().byId(sControlId),
+				iValue = Number(oControl.getValue());
+
+			fetch(this.sBaseUrl + '/' + route)
+				.then((response) => response.json())
+				.then((data) => {
+					iValue = iValue + data.rand;
+					if (+data.rand != 0) {
+						oControl.getParent().getParent().toggleStyleClass('tile-update');
+						setTimeout(() => {
+							oControl.getParent().getParent().toggleStyleClass('tile-update');
+						}, 200);
+					}
+					oControl.setValue(iValue);
+				})
+				.catch((e) => {
+					console.error('Ошибка при получении данных');
+					console.error(e);
+					clearInterval(timerID);
+				});
+		},
+
+		/**
+		 * Tiles' drag'n'drop logic
+		 */
+		onDrop(oEvent) {
+			let oTileContainer = oEvent.getSource().getParent(),
+				oDragged = oEvent.getParameter("draggedControl"),
 				oDropped = oEvent.getParameter("droppedControl"),
 				sInsertPosition = oEvent.getParameter("dropPosition"),
 				iDragPosition = oTileContainer.indexOfItem(oDragged),
@@ -52,69 +149,9 @@ sap.ui.define([
 			oTileContainer.insertItem(oDragged, iDropPosition);
 		},
 
-		_refreshTiles(arr) {
-			let counter = 1;
-			let timers = [];
-			arr.forEach((el) => timers.push(el.timer));
-
-			let index = 0;
-			let timerID = setInterval(() => {
-				if (!arr[index]) {
-					index = 0;
-				}
-				if (counter && counter % arr[index].timer === 0) {
-					this._fetchData(arr[index].route, arr[index].id);
-					index++;
-				}
-				if (counter >= Math.max(...timers)) {
-					counter = 1;
-				} else {
-					counter++;
-				}
-			}, 1000);
-		},
-
 		/**
-		 * Fetch data from express server
-		 *  @param {string} route - route to request data from
-		 *  @param {object} oControl - control which 'text' attr will get the fetched data
-		 *  @param {integer} interval - interval between requests in seconds
+		 * Navigation between tile groups
 		 */
-		_fetchData(route, sControlId) {
-			let oControl = this.getView().byId(sControlId);
-			let iValue = Number(oControl.getValue());
-
-			fetch(this.sBaseUrl + '/' + route)
-				.then((response) => response.json())
-				.then((data) => {
-					iValue = iValue + data.rand;
-					oControl.setValue(iValue);
-				})
-				.catch((e) => {
-					console.error('Ошибка при получении данных');
-					console.error(e);
-					clearInterval(timerID);
-				});
-		},
-		/* _fetchData(route, sControlId, interval) {
-			let oControl = this.getView().byId(sControlId);
-			let iValue = Number(oControl.getValue());
-
-			let timerID = setInterval(() => {
-				fetch(this.sBaseUrl + '/' + route)
-					.then(response => response.json())
-					.then(data => {
-						iValue = iValue + data.rand;
-						oControl.setValue(iValue);
-					})
-					.catch(e => {
-						console.error('Ошибка при получении данных');
-						console.error(e);
-						clearInterval(timerID);
-					});
-			}, interval * 1000);
-		}, */
-
 		onNavBtnClick(oEvent) {
 			this.aButtons.forEach((el) => el.setType(), this);
 			oEvent.getSource().setType('Attention');
